663d6f8ed6120d964d0e383f921c942e1d2afd58
Jonathan Poteet <teetopjon@gmail.com>
2024-09-25 19:21:56 -0400

    small db consumer changes to get it connected.

--- db_consumer.py
+++ db_consumer.py
@@ -4,16 +4,17 @@ import os
 import time
 from kafka import KafkaConsumer
 from pymongo import MongoClient
+import json
 
 #connect to kafka
-consumer = KafkaConsumer (bootstrap_servers="192.168.5.114:9092", auto_offset_reset='latest') # vm1
-consumer.subscribe (topics=["needTopic"])
+consumer = KafkaConsumer (bootstrap_servers="192.168.5.114:9092", auto_offset_reset='latest', value_deserializer=lambda m: json.loads(m.decode('utf-8')))  # Deserialize the message from bytes to JSON) # vm1
+consumer.subscribe (topics=["iot_images"])
 
 # connect to mongoDb
 uri = "mongodb://192.168.5.110:27017/"
 user = "team1"
 pw = "team1"
-client = MongoClient(uri, connectTimeoutMS=60000, tls=True, username=user, password=pw)
+client = MongoClient(uri, connectTimeoutMS=60000, tls=False, directConnection=True, username=user, password=pw)
 database = client["admin"]
 collection = database["iot_data"]
 
@@ -21,13 +22,13 @@ try:
 
     for msg in consumer:
 
-        print (str(msg.value, 'ascii')) #debugging
+        print(msg.value)  # debugging
         
         #todo: deserialize message
         collection.insert_one({
-            "id": msg.value.id,
-            "ground_truth": msg.value.GßroundTruth,
-            "data": msg.value.Data,
+            "id": msg.value.get("ID"),
+            "ground_truth": msg.value.get("GroundTruth"),
+            "data": msg.value.get("Data"),
             "inference": ""
         })
 
--- inference_consumer.py
+++ inference_consumer.py
@@ -7,7 +7,7 @@ from kafka import KafkaConsumer
 
 #connect to kafka
 consumer = KafkaConsumer (bootstrap_servers="192.168.5.114:9092", auto_offset_reset='latest') # vm1
-consumer.subscribe (topics=["needTopic"])
+consumer.subscribe (topics=["iot_images"])
 
 mlsURL = "192.168.5.110:port" #todo need this port number and possibly end point name
 
